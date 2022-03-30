ca74f5b2633115d60b7abe90545796b67855e767
tokutaro <tokuhiro_eto@yahoo.co.jp>
2022-03-30 10:59:05 +0900

    リファクタ完了

--- 15.3-sum.java
+++ 15.3-sum.java
@@ -6,22 +6,38 @@
 
 // @lc code=start
 class Solution {
+    /**
+     * [-2,0,1,1,2]
+     * i = -2, left = 0, right = 2
+     * i = -2, 0の時は増やすだけだと増えるが、減らせば0になる可能性が再び生じる
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> set = new HashSet<>();
         List<List<Integer>> result = new ArrayList<>();
         if (nums.length < 3) return result;
+        Arrays.sort(nums);
         int n = nums.length;
-        for (int i = 0; i < n-2; i++) {
-            for (int j = i+1; j < n-1; j++) {
-                for (int k = j+1; k < n; k++) {
-                    if (nums[i]+nums[j]+nums[k] == 0) {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        Collections.sort(list);
-                        set.add(list);
-                    }
+        for (int i = 0; i < n - 2; i++) {
+            int left = i+1;
+            int right = n-1;
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+                if (total < 0) {
+                    left++;
+                } else if (total > 0) {
+                    right--;
+                } else {
+                    // 0になったので結果に追加
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    set.add(list);
+                    // 0の場合にはleft,rightどっちも動かすことで0になる可能性が再びある
+                    left++;
+                    right--;
                 }
             }
         }
--- 16.3-sum-closest.java
+++ 16.3-sum-closest.java
@@ -13,16 +13,31 @@ class Solution {
         // [-9,10,4,1,3,8,5]
         // [-9,1,3,4,5,8,10]
         // num[i] + num[j] + num[k] ~ target
+        Arrays.sort(nums);
         int diff = Integer.MAX_VALUE;
+        int n = nums.length;
         int result = 0;
-        for (int i = 0; i < nums.length -2;i++) {
-            for (int j = i+1; j < nums.length-1; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if (Math.abs(target - sum) < diff) {
-                        result = sum;
-                        diff = Math.abs(target - sum);
+        for (int i = 0; i < n - 2;i++) {
+            int left = i+1;
+            int right = n-1;
+            while (left < right) {
+                int total = nums[i]+nums[left]+nums[right];
+                if (total < target) {
+                    if (diff > target-total) {
+                        result = total;
+                        diff = target-total;
                     }
+                    left++;
+                } else if (total > target) {
+                    if (diff > total - target) {
+                        result = total;
+                        diff = total - target;
+                    }
+                    right--;
+                } else {
+                    // ピタリ賞なので、これで終了
+                    System.out.println("pitari");
+                    return target;
                 }
             }
         }
