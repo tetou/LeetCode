fdd55e569426483e0a8fde069fd1e6c28e5f0d27
tokutaro <tokuhiro_eto@yahoo.co.jp>
2022-03-31 08:22:57 +0900

    AC stackを用いた解決方法

--- 32.longest-valid-parentheses.java
+++ 32.longest-valid-parentheses.java
@@ -7,24 +7,26 @@
 // @lc code=start
 class Solution {
     public int longestValidParentheses(String s) {
-        int maxans = 0;
-        int[] dp = new int[s.length()];
-
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == ')') {
-                if (s.charAt(i - 1) == '(') {
-                    dp[i] = (i>=2 ? dp[i-2]:0) + 2;
-                } else if (i - dp[i-1] > 0 && s.charAt(i - dp[i-1] - 1) == '(') {
-                    dp[i] = dp[i-1] + ((i-dp[i-1]) >= 2 ? dp[i-dp[i-1] -2] : 0) + 2;
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        // ((()))
+        // 10-1
+        // (()
+        // -1,0,2(閉じる)
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    result = Math.max(result, i - stack.peek());
                 }
             }
         }
-
-        for (int d : dp) {
-            maxans = Math.max(maxans, d);
-        }
-
-        return maxans;
+        return result;
     }
 }
 // @lc code=end
