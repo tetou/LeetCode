4d6388378fcd604cbc0072d6b5a529db2c86d7ff
tokutaro <tokuhiro_eto@yahoo.co.jp>
2022-04-02 12:18:38 +0900

    AC　自力。スタート地点をずらしていって計算量を削減

--- 77.combinations.java
+++ 77.combinations.java
@@ -13,7 +13,9 @@ class Solution {
         this.k = k; 
         this.result = new HashSet<List<Integer>>();
 
-        help(new int[n+1], new ArrayList<>());
+        for (int i = 1; i<= n-k+1; i++) {
+            help(i, new int[n+1], new ArrayList<>());
+        }
 
         List<List<Integer>> result2 = new ArrayList<List<Integer>>();
         for (List<Integer> element: result) {
@@ -28,20 +30,20 @@ class Solution {
      * used : すでに使われた数を管理する
      * list : 生成された組み合わせを引き継ぎ（一意性を保つためにsortしておく）
     */
-    private void help(int[] used, List<Integer> list) {
+    private void help(int start , int[] used, List<Integer> list) {
         if (list.size() == k) {
             Collections.sort(list);
             result.add(list);
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = start; i <= n; i++) {
             if (used[i] == 0) {
                 int[] tmp = Arrays.copyOf(used, n+1);
                 tmp[i] = 1;
                 ArrayList<Integer> tmp2 = new ArrayList<Integer>(list);
                 tmp2.add(i);
-                help(tmp, tmp2);
+                help(i, tmp, tmp2);
             }
         }
     }
